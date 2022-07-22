7b16ed31ff56f9ac8bd66a922d2f078ca517f924
David Steven Ramirez Osorio <david.dramirez@mercadolibre.com.co>
2022-07-22 16:52:06 -0500

    add other branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          In change this test
         </a>
       </header>
     </div>
