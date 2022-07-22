b4a493e844f0d454ab1eac9487ec11c303ac55d3
David Steven Ramirez Osorio <david.dramirez@mercadolibre.com.co>
2022-07-22 16:56:57 -0500

    add other branch 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi `coquimbin!
+         This is other change please
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          In change this test
+          Learn React
         </a>
       </header>
     </div>
